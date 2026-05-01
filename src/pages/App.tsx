58d47c68d85c0305d231de89a95627d311523c94
MiguelAraujo <miguel4araujo5@gmail.com>
2026-05-01 18:16:26 +0100

    Update App.tsx

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,22 @@
 import MACode from './MACode'
+import PortfolioPage from './PortfolioPage'
+
+function getPageFromPath() {
+  const path = window.location.pathname.replace(/\/+$/, '') || '/'
+
+  if (path === '/projetos') {
+    return 'portfolio'
+  }
+
+  return 'home'
+}
 
 export default function App() {
+  const page = getPageFromPath()
+
+  if (page === 'portfolio') {
+    return <PortfolioPage />
+  }
+
   return <MACode />
 }
