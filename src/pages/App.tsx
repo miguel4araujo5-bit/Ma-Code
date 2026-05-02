78eeb38e6dc38d3790bc82b4460d1c1ad915752b
MiguelAraujo <miguel4araujo5@gmail.com>
2026-05-02 08:08:46 +0100

    Update App.tsx

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,21 +1,45 @@
 import MACode from './MACode'
 import PortfolioPage from './PortfolioPage'
+import ServicePage from './ServicePage'
+import { servicePages, type ServicePageSlug } from '../data/servicePages'
 
-function getPageFromPath() {
-  const path = window.location.pathname.replace(/\/+$/, '') || '/'
+type AppPage =
+  | { type: 'home' }
+  | { type: 'portfolio' }
+  | { type: 'service'; slug: ServicePageSlug }
+
+function normalizePath(pathname: string) {
+  return pathname.replace(/\/+$/, '') || '/'
+}
+
+function getPageFromPath(): AppPage {
+  const path = normalizePath(window.location.pathname)
 
   if (path === '/projetos') {
-    return 'portfolio'
+    return { type: 'portfolio' }
   }
 
-  return 'home'
+  const servicePage = servicePages.find((page) => page.path === path)
+
+  if (servicePage) {
+    return {
+      type: 'service',
+      slug: servicePage.slug,
+    }
+  }
+
+  return { type: 'home' }
 }
 
 export default function App() {
   const page = getPageFromPath()
 
-  if (page === 'portfolio') {
+  if (page.type === 'portfolio') {
     return <PortfolioPage />
+  }
+
+  if (page.type === 'service') {
+    return <ServicePage slug={page.slug} />
   }
 
   return <MACode />
