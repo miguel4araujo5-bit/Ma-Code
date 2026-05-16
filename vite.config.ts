ba17ab3c8d910cd25d6bda2eb8684ae04b6c23ff
MiguelAraujo <miguel4araujo5@gmail.com>
2026-05-16 19:45:00 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    cssCodeSplit: true,
+    sourcemap: false,
+    reportCompressedSize: false,
+    chunkSizeWarningLimit: 700,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom']
+        }
+      }
+    }
+  }
 })
