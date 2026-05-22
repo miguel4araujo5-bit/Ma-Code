d92644074bb754be02ac6397b0bdf938b35a6c1e
MiguelAraujo <miguel4araujo5@gmail.com>
2026-05-22 19:44:00 +0100

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,8 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_WEB3FORMS_KEY: string
+  readonly VITE_WEB3FORMS_ACCESS_KEY?: string
+  readonly VITE_WEB3FORMS_KEY?: string
 }
 
 interface ImportMeta {
