43bc482460e264ba8addbe47a01e627255b4e1bd
MiguelAraujo <miguel4araujo5@gmail.com>
2026-07-10 00:44:38 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
 import App from './pages/App'
+import MACarteiraPage from './pages/MACarteiraPage'
+
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const path =
+  window.location.pathname.replace(/\/+$/, '') ||
+  '/'
+
+const RootPage =
+  path === '/produtos/ma-carteira'
+    ? MACarteiraPage
+    : App
+
+ReactDOM.createRoot(
+  document.getElementById('root')!
+).render(
   <React.StrictMode>
-    <App />
+    <RootPage />
   </React.StrictMode>
 )
