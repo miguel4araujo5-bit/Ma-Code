dd867074fccec294b4da953aa2b0860bb833da9b
MiguelAraujo <miguel4araujo5@gmail.com>
2026-09-05 23:32:21 +0100

    Protect historical lesson module assignments

--- src/components/ma-professor/lessons/lessonHistoricalEditSafety.ts
+++ src/components/ma-professor/lessons/lessonHistoricalEditSafety.ts
@@ -1,4 +1,5 @@
 import type {
+  EntityId,
   ISODate
 } from '../types'
 
@@ -20,5 +21,26 @@ export function assertLessonHistoricalDateChangeAllowed(
 
   throw new Error(
     'Esta aula já possui faltas ou avaliações. Para preservar o histórico, não é possível alterar a data através da edição normal da aula.'
+  )
+}
+
+export function assertLessonHistoricalModuleChangeAllowed(
+  previousModuleId: EntityId,
+  nextModuleId: EntityId,
+  attendanceCount: number,
+  assessmentCount: number
+) {
+  if (
+    previousModuleId === nextModuleId ||
+    (
+      attendanceCount === 0 &&
+      assessmentCount === 0
+    )
+  ) {
+    return
+  }
+
+  throw new Error(
+    'Esta aula já possui faltas ou avaliações. Para preservar o histórico, não é possível alterar a UFCD ou módulo através da edição normal da aula.'
   )
 }
