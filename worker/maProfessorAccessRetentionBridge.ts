0f2513bc02834ac80a967175a735a5a3417ddf25
MiguelAraujo <miguel4araujo5@gmail.com>
2026-09-12 23:09:00 +0100

    Compose login guard into MA-Professor access chain

--- worker/maProfessorAccessRetentionBridge.ts
+++ worker/maProfessorAccessRetentionBridge.ts
@@ -1,6 +1,6 @@
 import {
   MaProfessorAccessDurableObject as ExistingMaProfessorAccessDurableObject
-} from './maProfessorActivationCredentialGuardBridge'
+} from './maProfessorLoginAttemptGuardBridge'
 
 import {
   createMAProfessorAccessSessionSplitState
