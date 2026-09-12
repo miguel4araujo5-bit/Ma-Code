8bc3609d1099fda7cf06bbf4311fbb30f85609a1
MiguelAraujo <miguel4araujo5@gmail.com>
2026-09-12 23:25:10 +0100

    Wire public auth privacy guard into access chain

--- worker/maProfessorAccessRetentionBridge.ts
+++ worker/maProfessorAccessRetentionBridge.ts
@@ -1,6 +1,6 @@
 import {
   MaProfessorAccessDurableObject as ExistingMaProfessorAccessDurableObject
-} from './maProfessorLoginAttemptGuardBridge'
+} from './maProfessorPublicAuthPrivacyBridge'
 
 import {
   createMAProfessorAccessSessionSplitState
