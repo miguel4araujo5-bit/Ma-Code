1d2081c2aa2d1587c8447b27ed7838aa7c4413a1
MiguelAraujo <miguel4araujo5@gmail.com>
2026-09-13 11:19:50 +0100

    feat: mark regular annual curriculum components

--- src/components/ma-professor/types.ts
+++ src/components/ma-professor/types.ts
@@ -67,6 +67,7 @@ export interface ModuleUnit extends AuditFields {
   order: number
   plannedStartDate: ISODate | null
   plannedEndDate: ISODate | null
+  regularAnnual?: boolean
   active: boolean
 }
 
