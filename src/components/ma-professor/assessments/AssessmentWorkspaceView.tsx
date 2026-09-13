2460d8b4d071fc0f0e740dc069977ed4d5151623
MiguelAraujo <miguel4araujo5@gmail.com>
2026-09-13 14:29:29 +0100

    refactor(ma-professor): keep regular workspace router contract

--- src/components/ma-professor/assessments/AssessmentWorkspaceView.tsx
+++ src/components/ma-professor/assessments/AssessmentWorkspaceView.tsx
@@ -3,7 +3,7 @@ import type {
 } from '../types'
 
 import ProfessionalAssessmentWorkspaceView from './ProfessionalAssessmentWorkspaceView'
-import RegularAssessmentWorkspaceManagedView from './RegularAssessmentWorkspaceManagedView'
+import RegularAssessmentWorkspaceView from './RegularAssessmentWorkspaceManagedView'
 
 import type {
   AssessmentWorkspaceFilters,
@@ -33,7 +33,7 @@ export default function AssessmentWorkspaceView(
   return props.snapshot.selectedGroup
     ?.educationType === 'regular'
     ? (
-        <RegularAssessmentWorkspaceManagedView
+        <RegularAssessmentWorkspaceView
           {...props}
         />
       )
